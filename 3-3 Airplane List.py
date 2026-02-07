9b0fd6361b48287aea5df29dfe00e2dbc9f3127e
stcsb91 <stcsb91@gmail.com>
2026-02-07 17:12:10 -0500

    Add files via upload
    First instance of working with lists.

---
+++ 3-3 Airplane List.py
@@ -0,0 +1,3 @@
+airplanes = ['cessna', 'cirrus', 'beechcraft', 'backcountry', 'gulfstream', 'dassault', 'honda']
+message=f"the {airplanes[0].upper()} SKYHAWK was my first experience with flying an airplane!"
+print(message)
