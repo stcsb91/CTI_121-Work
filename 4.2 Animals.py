d89fcff74fa4339812ee0276fc8f53f782aa2b1f
Sam Starr-Baum <stcsb91@gmail.com>
2026-02-15 09:23:15 -0500

    4.2 Animals

---
+++ 4.2 Animals.py
@@ -0,0 +1,5 @@
+Southwestern_Animals = ('coyote', 'javelina', 'mountain lion', 'rattle snake')
+for animal in Southwestern_Animals:
+    print(f"The {animal.title()} is an animal I saw growing up in the southwestern part of the United States.")
+
+print("These species represent just a small amount of the incredibly diverse animal ecology of the American Southwest!")
