cb86915ad016be634020bdf64c60cad453b92586
Sam Starr-Baum <stcsb91@gmail.com>
2026-03-17 09:24:45 -0400

    Add files via upload

---
+++ 8.5 Cities.py
@@ -0,0 +1,11 @@
+#Happy St. Patricks Day!
+
+def describe_city(city, country="Ireland"):
+    print(f"The city, {city.title()}, is in the country of "
+          f"{country}.")
+
+describe_city("dublin")
+describe_city("cork")
+describe_city("galway")
+describe_city("kilkenny")
+describe_city("boston", 'USA')
